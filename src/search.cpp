fb24c06665f78bd6d122784646f6b4de38274e95
PaulJeFi <78982620+PaulJeFi@users.noreply.github.com>
2024-08-12 19:46:06 +0200

    Alpha beta fix
    This should be a correction of your alpha-beta function. To take with a grain of salt as I can't compile it on my machine.
    
    You will have to implement a quiescence search, and your Static Evaluation function must return a score relative to the side to being evaluated (because of the negamax framework).

--- src/search.cpp
+++ src/search.cpp
@@ -8,37 +8,39 @@
 
 
 int alphaBeta(Board *board, int depth, int alpha, int beta) {
-	if (depth == 0 || isGameOver(board)) {
-		return evaluate(board);
+
+	if (depth <= 0 || isGameOver(board)) {
+		return evaluate(board); // TODO : quiescence search
 	}
+
 	int value;
+	int bestScore = INT_MIN;
 	uint16_t moveArr[128];
 	uint16_t *moves = moveArr;
 	uint16_t *end = generateAllMoves(*board, moves);
 	uint16_t move;
-	if (board->turn == WHITE) {
-		value = INT_MIN;
-		for (int i = 0; i < end-moves; i++) {
-			move = *(moves+i);
-			makeMove(board, move);
-			value = std::max(value, alphaBeta(board, depth-1, alpha, beta));
-			alpha = std::max(alpha, value);
-			if (alpha >= beta) {
-				break;
+	Undo undo;
+	undo.halfMoveCounter = board->halfMoveCounter;
+	undo.castling = board->castling;
+	
+	// Move loop
+	for (int i = 0; i < end-moves; i++) {
+		move = *(moves+i);
+		undo.capturedPiece = getPieceAtSquare(board, getEndSquare(move));
+		makeMove(board, move);
+		value = alphaBeta(board, depth-1, -beta, -alpha);
+		unmakeMove(board, move, &undo);
+		alpha = std::max(alpha, value);
+		if (value > bestScore) {
+            bestScore = value;
+            if (value > alpha) {
+                alpha = value;
 			}
-		}
-	} else {
-		value = INT_MAX;
-		for (int i = 0; i < end-moves; i++) {
-			move = *(moves+i);
-			makeMove(board, move);
-			value = std::min(value, alphaBeta( board, depth-1, alpha, beta));
-			beta = std::min(beta, value);
-			if (beta <= alpha) {
-				break;
-			}
-		}
-	}
+        }
+        if( value >= beta )
+            break;
+   }
+   return bestScore;
 }
 
 
